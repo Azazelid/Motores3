f8f04a0fa09136c2c35754102a086af1601aa9be
Drolejan <drolejan@gmail.com>
2026-03-19 13:44:05 -0600

    astamina

--- Assets/Starter Assets/Runtime/ThirdPersonController/Scripts/PlayerStamina.cs
+++ Assets/Starter Assets/Runtime/ThirdPersonController/Scripts/PlayerStamina.cs
@@ -6,24 +6,36 @@ public class PlayerStamina : MonoBehaviour
     [SerializeField] private float currentStamina = 5f;
     [SerializeField] private float staminaDrain = 1f;
     [SerializeField] private float staminaRecovery = 0.5f;
+    bool sprintLock=false;
+    private float minStamina=1.5f;//Stamina minima para correr
 
     public float CurrentStamina => currentStamina;//Funcion que permite SOLO leer una variable privada
     public float MaxStamina => maxStamina;
 
     public bool CanSprint()
     {
-        return currentStamina > 0f;
+        return sprintLock==false && currentStamina > 0f;
     }
 
     public void DrainStamina()
     {
         currentStamina -= staminaDrain * Time.deltaTime;
         currentStamina = Mathf.Clamp(currentStamina, 0f, maxStamina);
+
+        if (currentStamina <= 0)
+        {
+            sprintLock=true;
+        }
     }
 
     public void RecoverStamina()
     {
         currentStamina += staminaRecovery * Time.deltaTime;
         currentStamina = Mathf.Clamp(currentStamina, 0f, maxStamina);
+
+        if (currentStamina > minStamina)
+        {
+            sprintLock=false;
+        }
     }
 }
